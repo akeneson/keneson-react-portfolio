9da517a71a9fb30a12ba5593915126c8d9ecd8dd
Amy Keneson <amy.l.keneson@icloud.com>
2020-11-26 16:24:31 -0600

    jumbtron added and working

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,10 +1,20 @@
 import React, { useState } from "react";
-import { Row, Container, Col } from "react-bootstrap"; 
+import { Row, Container, Col, Jumbotron } from "react-bootstrap"; 
  
 const Home = () => {
 
   return (
+    
     <Container>
+        <Jumbotron fluid>
+  <Container>
+    <h1>Fluid jumbotron</h1>
+    <p>
+      This is a modified jumbotron that occupies the entire horizontal space of
+      its parent.
+    </p>
+  </Container>
+</Jumbotron>
         <Row>
             <Col>column 1</Col>
             <Col>column 2</Col>
